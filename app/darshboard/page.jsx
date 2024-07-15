a81295ee53e207efcb2f5c064936444f22036414
Rijusmit Biswas <112485983+riju-talk@users.noreply.github.com>
2024-07-15 13:17:15 +0530

    new project init

--- app/darshboard/page.jsx
+++
@@ -1,5 +0,0 @@
-export default function default_dash(){
-    return <main>
-        <h1>Default Dashboard</h1>
-    </main>
-}
--- src/app/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Landing Page...</h1>
-    </main>
-  );
-}
